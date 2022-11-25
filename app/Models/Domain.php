5fbcab6edba13fedfbe2f7a78e362077a0e88f57
Yasin Köse <yasin.kose.123@gmail.com>
2022-11-25 17:09:07 +0300

    Add files relation in Domain Model

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Domain extends Model
 {
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'api_key',
+        'domain_ip'
+    ];
 
+    /**
+     * @return HasMany
+     */
+    public function files(): HasMany
+    {
+        return $this->hasMany(File::class);
+    }
 }
