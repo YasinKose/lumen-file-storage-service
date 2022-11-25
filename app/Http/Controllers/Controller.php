53693adebeff7a03dbb69247d6b4a141574b28bc
Yasin Köse <yasin.kose.123@gmail.com>
2022-11-25 19:11:58 +0300

    Remove unused params

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\ApiResponseTrait;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use ApiResponseTrait;
 }
